347a501abfea4a4a1bf769d5aa38b0adfc45fd41
dominovd <54228650+dominovd@users.noreply.github.com>
2026-06-09 21:27:55 +0300

    feat: add Google Search Console verification meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://roulettechatsites.com'),
+  verification: {
+    google: 'A9b_e0NLvrPSFKTm6mOipwnem5yfcY3GEJqoxAZQIDY',
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
